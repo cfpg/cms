a296c893f19da6d0105eb47aa4479a7cc622fad2
Carlos F. Peña <carlos@sutil.mx>
2016-05-22 18:22:40 -0700

    Adds base MVC files

---
+++ src/lib/Controller.js
@@ -0,0 +1,12 @@
+var _ = require('underscore');
+
+var Controller = {
+	initialize:function() {
+
+	},
+	getTemplate:function() {
+		return this.template;
+	}
+};
+
+module.exports = Controller;
---
+++ src/lib/View.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+
+var View = {
+	initialize:function() {
+
+	}
+};
+
+module.exports = View;
